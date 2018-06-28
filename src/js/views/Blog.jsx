abdeafad8820501e505924475be4f1d8134aa0e3
Emily Vong <emily.vong25@gmail.com>
2018-06-28 20:48:15 +0000

    jumbotron, sidebar and footer added to blog page

--- src/js/views/Blog.jsx
+++ src/js/views/Blog.jsx
@@ -3,15 +3,31 @@ import { Link } from "react-router-dom";
 
 import NavBar from '../component/navbar.jsx';
 import BlogJumbotron from '../component/BlogJumbotron.jsx';
+import BlogSidebar from '../component/BlogSidebar.jsx';
+import BlogFooter from '../component/BlogFooter.jsx';
 
 //create your first component
 export class Blog extends React.Component{
     
     render(){
         return (
-            <div className="container">
-                <NavBar />
-                <BlogJumbotron />
+            <div>
+                <div id="blogTopContentContainer" className="container">
+                    <NavBar />
+                    <BlogJumbotron />
+                    {/*card components will go here*/}
+                </div>
+                <div id="blogMainContentContainer" className="container">
+                    <div className="row">
+                        <div className="col-md-8">
+                            {/*sample blog post components will go here*/}
+                        </div>
+                        <aside className="col-md-4">
+                            <BlogSidebar />
+                        </aside>
+                    </div>
+                </div>
+                <BlogFooter />
             </div>
         );
     }
