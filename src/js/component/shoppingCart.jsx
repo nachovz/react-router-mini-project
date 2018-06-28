f55fe06e440f7a1fc0ceba8e787a70e388a2a7c8
Edgar Brignoni <edgarbrignoni@icloud.com>
2018-06-28 00:31:24 +0000

    test push

---
+++ src/js/component/shoppingCart.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//create your first component
+export class Checkout extends React.Component{
+    
+    render(){
+        return (
+            <div className="text-center mt-5">
+                <h1>Checkout</h1>
+
+                <p>Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with love!</p>
+            </div>
+        );
+    }
+}
