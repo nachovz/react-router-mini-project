a0555d39c8f660ebdc83159ea323f5ccd6cd0493
Mike Berrio <mikeberrio10@hotmail.com>
2018-06-28 00:34:34 +0000

    added code

--- src/js/views/Checkout.jsx
+++ src/js/views/Checkout.jsx
@@ -6,9 +6,7 @@ export class Checkout extends React.Component{
     render(){
         return (
             <div className="text-center mt-5">
-                <h1>Checkout</h1>
-
-                <p>Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with love!</p>
+                
             </div>
         );
     }
