c7a9a9c14b5960f61262bc511762a466fa4d9af7
Yariel <cubanboy4life@gmail.com>
2018-06-28 00:48:37 +0000

    initial commit

--- src/js/views/Home.jsx
+++ src/js/views/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-
+//Yariel, Frank & Raul
 //include images into your bundle
 import rigoImage from '../../img/rigo-baby.jpg';
 import NavBar from '../component/navbar.jsx';
