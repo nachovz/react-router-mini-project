98aff0303c558d1e962549fde177106ca1311103
Ignacio Cordoba <icordoba@4geeks.co>
2018-06-28 00:46:18 +0000

    Merging

--- src/js/views/Checkout.jsx
+++ src/js/views/Checkout.jsx
@@ -6,15 +6,11 @@ export class Checkout extends React.Component{
     render(){
         return (
             <div className="text-center mt-5">
-<<<<<<< HEAD
                 <h1>Checkout</h1>
 
                 <p>Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with love!</p>
                 
                 <h3>By</h3>
-=======
-                
->>>>>>> 9bf22d499ac92fa7639c21b0ad3d8e1e9bc2ae54
             </div>
         );
     }
