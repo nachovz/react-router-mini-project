ca2cedf7ebde5669b9cf3caba91992af53b67ce5
Edgar Brignoni <edgarbrignoni@icloud.com>
2018-06-29 22:49:41 +0000

    team2

--- src/js/views/Checkout.jsx
+++ src/js/views/Checkout.jsx
@@ -9,7 +9,7 @@ export class Checkout extends React.Component{
     render(){
         return (
             <React.Fragment>
-                <CheckoutHgti pusheader />
+                <CheckoutHeader />
 
                 <p>Made by Mike and Ed<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with love!</p>
             </React.Fragment>
