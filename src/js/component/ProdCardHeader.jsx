bd4b1fadd5e391067f5f6d6bd2b1d31adeee7e3f
Jesus <jpironag@gmail.com>
2018-06-30 19:18:59 +0000

    ProductCardHeader

--- src/js/component/ProdCardHeader.jsx
+++ src/js/component/ProdCardHeader.jsx
@@ -3,23 +3,15 @@ import 'bootstrap';
 import { Link } from "react-router-dom";
 
 function ProdCardHeader(props){
-    return (        
-        <div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-            <div className="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-                <div className="my-3 py-3">
-                    <h2 className="display-5">Another headline</h2>
-                    <p className="lead">And an even wittier subheading.</p>
-                </div>
-                <div className="cardLoop1 bg-light box-shadow mx-auto" >
-                    <style>{'div .cardLoop1 {width: 80%; height: 300px; border-radius: 21px 21px 0 0;'}</style>
-                </div>
+    return ( 
+        <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+            <div className="col-md-5 p-lg-5 mx-auto my-5">
+                <h1 className="display-4 font-weight-normal">Punny headline</h1>
+                <p className="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apples marketing pages.</p>
+                <a className="btn btn-outline-secondary" href="#">Coming soon</a>
             </div>
-            <div className="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-                <div className="my-3 p-3">
-                    <h2 className="display-5">Another headline</h2>
-                    <p className="lead">And an even wittier subheading.</p>
-                </div>
-            </div>
+            <div className="product-device box-shadow d-none d-md-block"></div>
+            <div className="product-device product-device-2 box-shadow d-none d-md-block"></div>
         </div>
         );
 }
