742c012cdc8a8af45d57b228c9434bf2f53dffca
Jesus <jpironag@gmail.com>
2018-06-30 18:01:28 +0000

    prodcardp

---
+++ src/js/component/ProdCardLoop.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import 'bootstrap';
+import { Link } from "react-router-dom";
+
+function ProdCardLoop(props){
+    return (
+        
+        <div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+            <div className="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+                <div className="my-3 py-3">
+                    <h2 className="display-5">Another headline</h2>
+                    <p className="lead">And an even wittier subheading.</p>
+                </div>
+                <div className="cardLoop1 bg-light box-shadow mx-auto" >
+                    <style>{'div .cardLoop1 {width: 80%; height: 300px; border-radius: 21px 21px 0 0;'}</style>
+                </div>
+            </div>
+            <div className="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div className="my-3 p-3">
+                    <h2 className="display-5">Another headline</h2>
+                    <p className="lead">And an even wittier subheading.</p>
+                </div>
+                <div className="cardLoop2 bg-dark box-shadow mx-auto">
+                    <style>{'div .cardLoop2 {width: 80%; height: 300px; border-radius: 21px 21px 0 0;'}</style>
+                </div>
+            </div>
+        </div>
+        );
+}
+
+export default ProdCardLoop;
--- src/js/views/Product.jsx
+++ src/js/views/Product.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap';
 import NavBar from '../component/navbar.jsx';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ProdCardLoop from '../component/ProdCardLoop.jsx';
 
 //Jobel and Jesus product page team 2
 
@@ -12,18 +13,20 @@ export class Product extends React.Component{
         return (
             <React.Fragment>
                 <NavBar />
-                <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-                    <div className="col-md-5 p-lg-5 mx-auto my-5">
-                        <h1 className="display-4 font-weight-normal">Punny headline</h1>
-                        <p className="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apples marketing pages.</p>
-                        <a className="btn btn-outline-secondary" href="#">Coming soon</a>
+                <div className="container">
+                    <style>{'div .container {max-width: 960px;'}</style>
+                    <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+                        <div className="col-md-5 p-lg-5 mx-auto my-5">
+                            <h1 className="display-4 font-weight-normal">Punny headline</h1>
+                            <p className="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apples marketing pages.</p>
+                            <a className="btn btn-outline-secondary" href="#">Coming soon</a>
+                        </div>
+                        <div className="product-device box-shadow d-none d-md-block"></div>
+                        <div className="product-device product-device-2 box-shadow d-none d-md-block"></div>
                     </div>
-                    <div className="product-device box-shadow d-none d-md-block"></div>
-                    <div className="product-device product-device-2 box-shadow d-none d-md-block"></div>
                 </div>
-                <div className="text-center mt-5">
-                    <h1>Product</h1>
-                    <p>Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with love!</p>
+                <div id="prodCardContentContainer">
+                    <ProdCardLoop />
                 </div>
             </React.Fragment>
         );
