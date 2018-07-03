49701be2ecd1d0923238dbcbb523dc4c972ca1da
Edgar Brignoni <edgarbrignoni@icloud.com>
2018-07-03 01:24:08 +0000

    team2

--- src/js/component/CheckoutCart.jsx
+++ src/js/component/CheckoutCart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import {Consumer} from "../stores/AppContext.jsx";
 
 export default class CheckoutCart extends React.Component{
     constructor() {
