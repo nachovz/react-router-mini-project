49eb34345ad28c15a5b8472067aec982090e7285
Frank Izquierdo <frankdavid93om@yahoo.com>
2018-07-09 23:04:51 +0000

    button adds products to cart

--- src/js/component/home/products.jsx
+++ src/js/component/home/products.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import {Consumer} from "../../stores/AppContext.jsx";
+import PropTypes from 'prop-types';
 
 export class Products extends React.Component{
     
@@ -8,18 +9,38 @@ export class Products extends React.Component{
         return (
             <Consumer>
                 {({ state }) => (
-                state.products.map((items, index) => {
-                return (
-                    <div className="col-lg-4 text-center" key={index}>
-                        <img className="rounded-circle" src={items.image_url} alt="Generic placeholder image" width="140" height="140"/>
-                        <h2>{items.name}</h2>
-                        <p>{items.content}</p>
-                        <p>{items.description}</p>
-                        <p><a className="btn btn-secondary" href="#" role="button">&#36;{items.price} »</a></p>
-                    </div>
-                    );
-            })
-            )}
+                    state.products.map((items, index) => {
+                        return (
+                            <div className="col-lg-4 text-center" key={index}>
+                                <img className="rounded-circle" src={items.image_url} alt="Generic placeholder image" width="140" height="140"/>
+                                <h2>{items.name}</h2>
+                                <p>{items.content}</p>
+                                <p>{items.description}</p>
+                                <Button 
+                                    price={items.price}
+                                    idOfProduct={items.id}
+                                />
+                            </div>
+                        );
+                    })
+                )}
             </Consumer>
     );}
 }
+
+function Button(props) {
+    return(
+        <Consumer>
+            {
+                ({actions}) => {
+                    return <p><a className="btn btn-secondary" href="#" role="button" onClick={() => actions.addProductToCart(props.idOfProduct)}>&#36;{props.price} »</a></p>;
+                }
+            }
+        </Consumer>
+        );
+}
+
+Button.propTypes = {
+  price: PropTypes.number,
+  idOfProduct: PropTypes.nimber
+};
